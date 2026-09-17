8f753ae5da22c1be0fdf1cc9e111e58a346725fe
shreyasmkhandekar-lab <shreyasmkhandekar@gmail.com>
2026-09-17 20:10:56 +0530

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.java
@@ -0,0 +1,41 @@
+public class Solution {
+    private List<Integer> output = new ArrayList<>();
+    private int[] indegree;
+    private List<List<Integer>> adj;
+
+    private void dfs(int node) {
+        output.add(node);
+        indegree[node]--;
+        for (int nei : adj.get(node)) {
+            indegree[nei]--;
+            if (indegree[nei] == 0) {
+                dfs(nei);
+            }
+        }
+    }
+
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        adj = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            adj.add(new ArrayList<>());
+        }
+        indegree = new int[numCourses];
+        for (int[] pre : prerequisites) {
+            indegree[pre[0]]++;
+            adj.get(pre[1]).add(pre[0]);
+        }
+
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                dfs(i);
+            }
+        }
+
+        if (output.size() != numCourses) return new int[0];
+        int[] res = new int[output.size()];
+        for (int i = 0; i < output.size(); i++) {
+            res[i] = output.get(i);
+        }
+        return res;
+    }
+}
